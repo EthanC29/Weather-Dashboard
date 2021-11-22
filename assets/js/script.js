c83f8e1ed386efed5fc37313d8d690709d625437
Ethan <90936828+EthanC29@users.noreply.github.com>
2021-11-21 19:47:07 -0500

    trying to refactor code to be more concise

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,3 @@
-let forecastWeek = [];
-
-
-
 function getApi(lat, lon) {
     var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&exclude=minutely,hourly&appid=ce475acf48140382619c0453c95cfcf8';
 
@@ -16,21 +12,26 @@ function getApi(lat, lon) {
 
         console.log();
 
-        forecastWeek = [
-            {
-                "day": String(new Date(data.daily[0].dt * 1000)).slice(0,3),
-                "date": data.daily[0].dt,
-                "icon": "",
-                "tempMin": data.daily[0].temp.min,
-                "tempMax": data.daily[0].temp.max,
-                "wind": data.daily[0].wind_speed,
-                "humidity": data.daily[0].humidity,
-                "uvindex": data.daily[0].uvi
-            },
-            {
+        let forecastWeek = []
+
+        for (var i = 0; i < 6; i++) {
+
+            forecastWeek[i] = {
+                "day": String(new Date(data.daily[i].dt * 1000)).slice(0,3),
+                "date": new Date(data.daily[i].dt * 1000),
+                "icon": data.daily[i].weather.icon,
+                "tempMin": data.daily[i].temp.min,
+                "tempMax": data.daily[i].temp.max,
+                "wind": data.daily[i].wind_speed,
+                "humidity": data.daily[i].humidity,
+                "uvindex": data.daily[i].uvi
+            }
+
+        }
+            /*{
                 "day": String(new Date(data.daily[1].dt * 1000)).slice(0,3),
                 "date": data.daily[1].dt,
-                "icon": "",
+                "icon": data.daily[1].weather.icon,
                 "tempMin": data.daily[1].temp.min,
                 "tempMax": data.daily[1].temp.max,
                 "wind": data.daily[1].wind_speed,
@@ -40,7 +41,7 @@ function getApi(lat, lon) {
             {
                 "day": String(new Date(data.daily[2].dt * 1000)).slice(0,3),
                 "date": data.daily[2].dt,
-                "icon": "",
+                "icon": data.daily[2].weather.icon,
                 "tempMin": data.daily[2].temp.min,
                 "tempMax": data.daily[2].temp.max,
                 "wind": data.daily[2].wind_speed,
@@ -50,7 +51,7 @@ function getApi(lat, lon) {
             {
                 "day": String(new Date(data.daily[3].dt * 1000)).slice(0,3),
                 "date": data.daily[3].dt,
-                "icon": "",
+                "icon": data.daily[3].weather.icon,
                 "tempMin": data.daily[3].temp.min,
                 "tempMax": data.daily[3].temp.max,
                 "wind": data.daily[3].wind_speed,
@@ -60,7 +61,7 @@ function getApi(lat, lon) {
             {
                 "day": String(new Date(data.daily[4].dt * 1000)).slice(0,3),
                 "date": data.daily[4].dt,
-                "icon": "",
+                "icon": data.daily[4].weather.icon,
                 "tempMin": data.daily[4].temp.min,
                 "tempMax": data.daily[4].temp.max,
                 "wind": data.daily[4].wind_speed,
@@ -70,25 +71,23 @@ function getApi(lat, lon) {
             {
                 "day": String(new Date(data.daily[5].dt * 1000)).slice(0,3),
                 "date": data.daily[5].dt,
-                "icon": "",
+                "icon": data.daily[5].weather.icon,
                 "tempMin": data.daily[5].temp.min,
                 "tempMax": data.daily[5].temp.max,
                 "wind": data.daily[5].wind_speed,
                 "humidity": data.daily[5].humidity,
                 "uvindex": data.daily[5].uvi
-            },
-        ];
+            }*/
+        
 
-        localStorage.setItem("forecast", JSON.stringify(forecastWeek));
+        localStorage.setItem("forecastWeek", JSON.stringify(forecastWeek));
       
     });
 }
 
 getApi(43.79, -79.20);
 
-
-
-
+/*
 $("#btn").click(function(){
     var geocoder = new google.maps.Geocoder();
     geocoder.geocode( { 'address': $("#search-bar").value}, function(results, status) {
@@ -97,3 +96,32 @@ $("#btn").click(function(){
         }
     });
 });
+*/
+
+function appendData() {
+    forecastWeek = JSON.parse(localStorage.getItem("forecastWeek"));
+    console.log(forecastWeek);
+
+    for (var i = 0; i < forecastWeek.Length; i++) {
+
+        let cityName = "<City Name>";
+        let dateEl = new Date(forecastWeek[i].date * 1000);
+        let tempEl = "Avg. Temp: " + (Math.round(((forecastWeek[i].tempMax + forecastWeek[i].tempMin)/2) * 10) / 10) + "° F";
+        let windEl = "Wind: " + forecastWeek[i].wind + " MPH";
+        let humidEl = "Humidity: " + forecastWeek[i].humidity + "%";
+        let uviEl = "UV Index: " + forecastWeek[i].uvindex;
+
+        $(".city").append();
+        $(".current-temp").append(tempEl);
+        $(".current-wind").append(windEl);
+        $(".current-humid").append(humidEl);
+        $(".current-uvi").append(uviEl);
+
+    }
+
+
+
+}
+
+
+appendData();
